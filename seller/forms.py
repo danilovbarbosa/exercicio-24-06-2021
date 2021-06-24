efbca1cce52de10fe9671e2ef1488e279dee27f9
Danilo Costa <danilo.v.barbosa@gmail.com>
2021-06-24 16:34:42 -0300

    feat(tests_api): script de teste para api.

--- seller/forms.py
+++ seller/forms.py
@@ -1,10 +1,34 @@
-from django.http import request, response
+from django import forms
+
+# from django.http import request, response
+
+import requests
 
 from django.forms import ModelForm
 from seller.models import Seller
 
 
+# def get_produtc():
+#     r = requests.get("http://172.28.0.3:8000/product/")
+#     produtos_dict = r.json()
+    
+#     print(produtos_dict)
+
 class SellerForm(ModelForm):
+
     class Meta:
         model = Seller
         fields = "__all__"
+        
+        PRODUCTS_CHOICES = [("a", "a")]
+        get_produtc()
+        
+        widgets = {
+            "product": forms.MultipleChoiceField(
+                required=False,
+                widget=forms.CheckboxSelectMultiple,
+                choices=PRODUCTS_CHOICES,
+            ),
+        }
+
+
---
+++ testes_api.py
@@ -0,0 +1,71 @@
+import requests
+
+url_base = "http://172.28.0.3:8000/"
+
+
+def get_produtc():
+    url_test = url_base + "product/"
+    r = requests.get(url_test)
+    produtos_dict = r.json()
+
+    print(produtos_dict)
+
+
+def post_produtc():
+    url_test = url_base + "product/"
+    objeto = {
+        "name": "test oi",
+        "description": "test oi",
+        "price": 11.0,
+        "category": 1,
+    }
+    r = requests.post(url_test, data=objeto)
+
+    produtos_dict = r.json()
+
+    print(produtos_dict)
+
+
+def put_produtc():
+    url_test = url_base + "product/1/"
+    objeto = {
+        "name": "test put",
+        "description": "test put",
+        "price": 141.0,
+        "category": 1,
+    }
+    r = requests.put(url_test, data=objeto)
+
+    produtos_dict = r.json()
+
+    print(produtos_dict)
+
+
+def patch_produtc():
+    url_test = url_base + "product/2/"
+    objeto = {
+        "name": "test patch",
+    }
+    r = requests.patch(url_test, data=objeto)
+
+    produtos_dict = r.json()
+
+    print(produtos_dict)
+
+
+def delete_produtc():
+    url_test = url_base + "product/3/"
+
+    r = requests.delete(url_test)
+
+    produtos_dict = r.status_code
+
+    print(produtos_dict)
+
+get_produtc()
+post_produtc()
+put_produtc()
+patch_produtc()
+delete_produtc()
+get_produtc()
+
